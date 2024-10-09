98e717b158c43af4faf05a587ee86ec938bd82bc
phewson16 <peterhewson16@gmail.com>
2024-10-09 12:09:07 -0400

    final push

--- js/index.js
+++ js/index.js
@@ -2,21 +2,27 @@ const spellBook = ['Expelliarmus', 'Accio', 'Expecto Patronum', 'Wingardium Levi
 let mp = 50;
 
 const castSpell = () => {
-    console.log('spell casted');
-    // choose a random spell from the array
-    const randomIndex = Math.floor(Math.random() * spellBook.length);
-    const randomSpell = spellBook[randomIndex];
-
-    //show the user the spell on the screen
+    // keep constants outside of if-statement
     const spellElement = document.getElementById('spell');
-    const message = `You cast the ${randomSpell} spell`;
-    spellElement.textContent = randomSpell;
-
-    // subtract 5 mana points from the total
-    mp = mp - 5;
-    console.log('Mana', mp);
-
-    // show the user the new mana points on the screen
     const manaElement = document.getElementById('mana-points');
-    manaElement.textContent = mp;
+
+    // if statement to stop when reach 0 mana
+    if (mp > 0) {
+        // choose a random spell from the array
+        const randomIndex = Math.floor(Math.random() * spellBook.length);
+        const randomSpell = spellBook[randomIndex];
+
+        // show the user the spell on the screen
+        const message = `You cast the ${randomSpell} spell`;
+        spellElement.textContent = message;
+
+        // subtract 5 mana points from the total
+        mp = mp - 5;
+
+        // show the user the new mana points on the screen
+        manaElement.textContent = mp;
+    } else {
+        // Show a message if out of mana
+        spellElement.textContent = "Sorry, you have run out of mana!";
+    }
 }
